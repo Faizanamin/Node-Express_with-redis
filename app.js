60475b70172d475a089c31df6e4c540131cbc71e
faizan amin <faizanamin20@gmail.com>
2018-09-11 01:23:05 +0500

    simple app with redis

---
+++ app.js
@@ -0,0 +1,87 @@
+let express = require("express");
+let bodyParser = require("body-parser");
+let exphbs = require('express-handlebars');
+let redis = require('redis');
+let path = require('path');
+let methodOverride = require("method-override");
+
+//create Redis Client
+
+let client =redis.createClient();
+client.on('connect',function(){
+    console.log('Conneted To Redis Succesfully');
+});
+
+//set port 
+const port = 4200;
+//init app
+let app = express();
+
+// view engine 
+app.engine('handlebars',exphbs({defaultLayout:'main'}));
+app.set('view engine','handlebars');
+
+// //body-parser
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({extended:false}));
+
+//methodoverride for delete request
+app.use(methodOverride('_method'));
+
+// Route for main page
+app.get('/',function(req,res){
+    res.render('searchusers');
+});
+// search route
+app.post('/user/search',function(req,res,next){
+    let id = req.body.id;
+    client.hgetall(id,function(err,obj){
+        if(!obj){
+            res.render('searchusers',{
+                error:'User Dose Not Exist'
+            });
+        }else {
+            obj.id = id ;
+            res.render('details',{
+                user :obj
+            })
+        }
+    });
+});
+
+//Add user LInk 
+app.get('/addUser',function(req,res){
+       res.render('addusers');
+});
+
+// Add user to database
+app.post('/user/add',function(req,res){
+    let id = req.body.id;
+    let first_name = req.body.first_name;
+    let last_name = req.body.last_name;
+    let email = req.body.email;
+    let phone = req.body.phone;
+    client.HMSET(id,[
+        'first_name',first_name,
+        'last_name',last_name,
+        'email',email,
+        'phone',phone
+    ],function(err,rply){
+        if(err){
+            Console.log(err);
+        }
+        console.log(rply);
+        res.redirect('/');
+
+    });
+});
+//Delete A user
+app.delete('/user/delete/:id',function(req,res){
+client.del(req.params.id);
+res.redirect('/')
+});
+//Run server
+app.listen(port,function(){
+    console.log('server running on port '+port);
+});
+
